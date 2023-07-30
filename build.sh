0b9a48c734b67802465b6246f7dd3b6e93ff8fec
shaovie <43458943@qq.com>
2023-07-30 22:58:27 +0800

    编译全源码+测试代码和示例代码

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 # sh build.sh map
 # sh build.sh
 GOOS=linux GOARCH=amd64 go build -tags $1 goev
+GOOS=linux GOARCH=amd64 go build -o /dev/null example/techempower.go
+GOOS=linux GOARCH=amd64 go build -o /dev/null example/reuseport.go
+GOOS=linux GOARCH=amd64 go test -o /dev/null -c .
