649fab233d5315a78d9a5de0786a4d1bb466fa3c
shaovie <43458943@qq.com>
2023-08-07 01:02:08 +0800

    clean code

--- reactor.go
+++ reactor.go
@@ -1,6 +1,6 @@
 package goev
 
-// Autor cuisw. 2023.06
+// Autor cuisw. 2023.07
 
 import (
 	"errors"
@@ -47,9 +47,6 @@ func NewReactor(opts ...Option) (*Reactor, error) {
 // AddEvHandler can register a file descriptor (fd) and its corresponding handler object into the Reactor.
 // If multiple evPool instances are specified internally, the fd will be rotated to the designated
 // evPool instance based on fd % idx.
-//
-// AddEvHandler可以将fd及其对应的处理对象注册到Reactor中，如果内部指定了多个evpoll，
-// 那么会根据fd %idx 轮转到指定的evpoll中
 func (r *Reactor) AddEvHandler(eh EvHandler, fd int, events uint32) error {
 	if fd < 1 || eh == nil { // NOTE fd must > 0
 		return errors.New("AddEvHandler: invalid params")
@@ -72,31 +69,6 @@ func (r *Reactor) RemoveEvHandler(eh EvHandler, fd int) error {
 	}
 	return errors.New("ev handler not add")
 }
-
-// ScheduleTimer starts a timer that can be either one-time execution or repeated execution
-// ScheduleTimer 启动一个定时器，可以是执行一次的，也可以是循环执行的
-//
-// delay, interval are both relative time measurements with millisecond accuracy, for example, delay=5msec.
-// MUST: One EvHandler can only register one timer
-//func (r *Reactor) ScheduleTimer(eh EvHandler, delay, interval int64) error {
-//	i := 0
-//	if r.evPollNum > 1 {
-//		if ep := eh.getEvPoll(); ep != nil {
-//			return ep.scheduleTimer(eh, delay, interval)
-//		}
-//		i = int(r.timerIdx.Add(1) % int64(r.evPollNum))
-//	}
-//	return r.evPolls[i].scheduleTimer(eh, delay, interval)
-//}
-
-// CancelTimer cancel an timer bound to eh
-//func (r *Reactor) CancelTimer(eh EvHandler) {
-//	if eh != nil {
-//		if ep := eh.getEvPoll(); ep != nil {
-//			ep.cancelTimer(eh)
-//		}
-//	}
-//}
 
 // Run starts the multi-event evpolling to run.
 func (r *Reactor) Run() error {
