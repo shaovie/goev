e7fba43b7eb248b171d7e27c8fe7d1d910e8f221
shaovie <43458943@qq.com>
2023-08-17 22:20:52 +0800

    mempool bench

--- mempool_test.go
+++ mempool_test.go
@@ -35,35 +35,65 @@ func TestMempool(t *testing.T) {
 	fmt.Println(MemPoolStat())
 }
 func BenchmarkMempool(b *testing.B) {
-	mbL := make([][]byte, 4096)
-	for i := 0; i < b.N; i++ {
-		for i := 0; i < 4096; i++ {
-			mb := Malloc(int(rand.Int63()%4096) + 16)
-			//mb := Malloc(2048 + int(rand.Int63() % 256))
-			mbL[i] = mb
-			mb[len(mb)-1] = 'a'
-		}
-		for i := range mbL {
-			Free(mbL[i])
-		}
+	cases := []struct {
+		name string
+		N    int64
+	}{
+		{"N-1k", 1024 * 1},
+		{"N-4k", 1024 * 4},
+		{"N-8k", 1024 * 8},
+		{"N-16k", 1024 * 16},
+		{"N-32k", 1024 * 32},
+		{"N-64k", 1024 * 64},
+	}
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			mbL := make([][]byte, 4096)
+			for i := 0; i < b.N; i++ {
+				for i := 0; i < len(mbL); i++ {
+					mb := Malloc(int(rand.Int63()%(c.N-16)) + 16)
+					//mb := Malloc(2048 + int(rand.Int63() % 256))
+					mbL[i] = mb
+					mb[len(mb)-1] = 'a'
+				}
+				for i := range mbL {
+					Free(mbL[i])
+				}
+			}
+		})
 	}
 }
 func BenchmarkMake(b *testing.B) {
-	mbL := make([][]byte, 4096)
-	for i := 0; i < b.N; i++ {
-		for i := 0; i < 4096; i++ {
-			if true { // in heap
-				mb := make([]byte, int(rand.Int63()%4096)+16)
-				//mb := make([]byte, 2048 + int(rand.Int63()%256))
-				mbL[i] = mb
-				mb[len(mb)-1] = 'a'
-			} else { // in stack
-				mb := make([]byte, 2098)
-				mb[len(mb)-1] = 'a'
+	cases := []struct {
+		name string
+		N    int64
+	}{
+		{"N-1k", 1024 * 1},
+		{"N-4k", 1024 * 4},
+		{"N-8k", 1024 * 8},
+		{"N-16k", 1024 * 16},
+		{"N-32k", 1024 * 32},
+		{"N-64k", 1024 * 64},
+	}
+	for _, c := range cases {
+		b.Run(c.name, func(b *testing.B) {
+			mbL := make([][]byte, 4096)
+			for i := 0; i < b.N; i++ {
+				for i := 0; i < len(mbL); i++ {
+					if true { // in heap
+						mb := make([]byte, int(rand.Int63()%(c.N-16))+16)
+						//mb := make([]byte, 2048 + int(rand.Int63()%256))
+						mbL[i] = mb
+						mb[len(mb)-1] = 'a'
+					} else { // in stack
+						mb := make([]byte, 2098)
+						mb[len(mb)-1] = 'a'
+					}
+				}
+				for i := range mbL {
+					mbL[i] = nil
+				}
 			}
-		}
-		for i := range mbL {
-			mbL[i] = nil
-		}
+		})
 	}
 }
