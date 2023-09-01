b3098d1ec0341e28961776c021ab998fd671cbb6
shaovie <43458943@qq.com>
2023-09-01 13:50:46 +0800

    make Options private

--- options.go
+++ options.go
@@ -17,8 +17,8 @@ func SetIOBuffUsePool(v bool) {
 	})
 }
 
-// Options provides all optional parameters within the framework
-type Options struct {
+// options provides all optional parameters within the framework
+type options struct {
 	// acceptor options
 	reuseAddr     bool // SO_REUSEADDR
 	reusePort     bool // SO_REUSEPORT
@@ -42,11 +42,11 @@ type Options struct {
 }
 
 // Option function
-type Option func(*Options)
+type Option func(*options)
 
-func setOptions(optL ...Option) Options {
+func setOptions(optL ...Option) options {
 	//= defaut options
-	opts := Options{
+	opts := options{
 		reuseAddr:         true,
 		reusePort:         false,
 		evPollNum:         1,
@@ -67,7 +67,7 @@ func setOptions(optL ...Option) Options {
 
 // ReuseAddr for SO_REUSEADDR
 func ReuseAddr(v bool) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		o.reuseAddr = v
 	}
 }
@@ -78,21 +78,21 @@ func ReuseAddr(v bool) Option {
 // Please make sure you have a good understanding of SO_REUSEPORT.(man 7 socket)
 // For example code, please refer to example/reuseport.go
 func ReusePort(v bool) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		o.reusePort = v
 	}
 }
 
 // ListenBacklog For syscall.listen(fd, backlog), also affect `for i < backlog/2 { syscall.accept() }`
 func ListenBacklog(v int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		o.listenBacklog = v
 	}
 }
 
 // SockRcvBufSize for SO_RCVBUF, for new sockfd in acceptor/connector
 func SockRcvBufSize(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		o.sockRcvBufSize = n
 	}
 }
@@ -101,7 +101,7 @@ func SockRcvBufSize(n int) Option {
 // 主要根据自己的服务中fd并发数量(fd=0~n的范围)来定
 // fd数量超过此值并不会拒绝服务, 只是存储结构切换到map
 func EvFdMaxSize(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		if n > 0 {
 			o.evFdMaxSize = n
 		}
@@ -114,7 +114,7 @@ func EvFdMaxSize(n int) Option {
 //
 // EvPollLockOSThread 是否绑定固定线程 请参考go doc runtime.LockOSThread (经过实测, 会降低约2%的性能)
 func EvPollLockOSThread(v bool) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		o.evPollLockOSThread = v
 	}
 }
@@ -127,7 +127,7 @@ func EvPollLockOSThread(v bool) Option {
 // EvPollNum evpoll数量, 每个evpoll就像是在独立的线程中运行, 建议用CPUx2-1(注意留出其他goroutine的cpu)
 // 网络程序I/O密集, cpu切换会比较频繁, 所以1个cpu绑定2个evpoll最好
 func EvPollNum(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		if n > 0 {
 			o.evPollNum = n
 		}
@@ -140,7 +140,7 @@ func EvPollNum(n int) Option {
 //
 // EvReadyNum evpoll一次轮询获取数量n的Ready I/O事件, 有利于提高批量处理能力, 太大容易影响新事件的处理
 //func EvReadyNum(n int) Option {
-//	return func(o *Options) {
+//	return func(o *options) {
 //		if n > 0 {
 //			o.evReadyNum = n
 //		}
@@ -155,7 +155,7 @@ func EvPollNum(n int) Option {
 // 单个evpoll内的全局共享内存, 对cpu cache 友好, 读取socket缓存区的数据时非常高效
 // 另: 如果是Epoll-ET模式, 就需要有足够大的内存来一次性读完缓冲区的数据
 func EvPollReadBuffSize(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		if n > 0 {
 			o.evPollReadBuffSize = n
 		}
@@ -164,7 +164,7 @@ func EvPollReadBuffSize(n int) Option {
 
 // EvPollWriteBuffSize is the global shared memory within a single evpoll,
 func EvPollWriteBuffSize(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		if n > 0 {
 			o.evPollWriteBuffSize = n
 		}
@@ -176,14 +176,14 @@ func EvPollWriteBuffSize(n int) Option {
 //
 // Turn off cache time if `period' equal zero
 //func EvPollCacheTimePeriod(period int) Option {
-//	return func(o *Options) {
+//	return func(o *options) {
 //		o.evPollCacheTimePeriod = period
 //	}
 //}
 
 // TimerHeapInitSize is the initial array size of the heap structure used to implement timers
 func TimerHeapInitSize(n int) Option {
-	return func(o *Options) {
+	return func(o *options) {
 		if n > 0 {
 			o.timerHeapInitSize = n
 		}
